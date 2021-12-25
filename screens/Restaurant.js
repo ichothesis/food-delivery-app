41237f491b4e165515d12a413e1b9310e0515830
Farhan Yakub Bawazir <farhan.bawazir16@gmail.com>
2021-12-25 12:08:33 +0800

    restaurant menu scrollview

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -74,7 +74,78 @@ export default function Restaurant({route, navigation}) {
     );
   };
 
-  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
+  const renderFoodInfo = () => {
+    return (
+      <Animated.ScrollView
+        horizontal
+        pagingEnabled
+        scrollEventThrottle={16}
+        snapToAlignment={'center'}
+        showsHorizontalScrollIndicator={false}
+        //onScroll={}
+      >
+        {restaurant.menu.map((item, index) => (
+          <View key={index} style={{alignItems: 'center'}}>
+            <View style={{height: SIZES.height * 0.35}}>
+              <Image
+                source={item.photo}
+                resizeMode="cover"
+                style={{width: SIZES.width, height: '100%'}}
+              />
+              <View
+                style={{
+                  position: 'absolute',
+                  bottom: -20,
+                  width: SIZES.width,
+                  height: 50,
+                  justifyContent: 'center',
+                  flexDirection: 'row',
+                }}>
+                <TouchableOpacity
+                  style={{
+                    width: 50,
+                    backgroundColor: COLORS.white,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderTopLeftRadius: 25,
+                    borderBottomLeftRadius: 25,
+                  }}>
+                  <Text style={{...FONTS.body1}}>-</Text>
+                </TouchableOpacity>
+                <View
+                  style={{
+                    width: 50,
+                    backgroundColor: COLORS.white,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <Text style={{...FONTS.h2}}>5</Text>
+                </View>
+                <TouchableOpacity
+                  style={{
+                    width: 50,
+                    backgroundColor: COLORS.white,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderTopRightRadius: 25,
+                    borderBottomRightRadius: 25,
+                  }}>
+                  <Text style={{...FONTS.body1}}>+</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        ))}
+      </Animated.ScrollView>
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {renderHeader()}
+      {renderFoodInfo()}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
