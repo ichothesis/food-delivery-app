f9e2fbb247268a3b91df6ff64859a000456b592b
Farhan Yakub Bawazir <farhan.bawazir16@gmail.com>
2021-12-27 12:04:30 +0800

    restaurant order modal

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -231,7 +231,64 @@ export default function Restaurant({route, navigation}) {
             backgroundColor: COLORS.white,
             borderTopLeftRadius: 40,
             borderTopRightRadius: 40,
-          }}></View>
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: SIZES.padding * 2,
+              paddingHorizontal: SIZES.padding * 3,
+              borderBottomColor: COLORS.lightGray2,
+              borderBottomWidth: 1,
+            }}>
+            <Text style={{...FONTS.h3}}>Items in cart</Text>
+            <Text style={{...FONTS.h3}}>$45</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: SIZES.padding * 2,
+              paddingHorizontal: SIZES.padding * 3,
+            }}>
+            <View style={{flexDirection: 'row'}}>
+              <Image
+                resizeMode="contain"
+                source={icons.pin}
+                style={{width: 20, height: 20, tintColor: COLORS.darkgray}}
+              />
+              <Text style={{marginLeft: SIZES.padding, ...FONTS.h4}}>
+                Location
+              </Text>
+            </View>
+
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Image
+                source={icons.master_card}
+                resizeMode="contain"
+                style={{width: 20, height: 20, tintColor: COLORS.darkgray}}
+              />
+              <Text style={{marginLeft: SIZES.padding, ...FONTS.h4}}>8888</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              padding: SIZES.padding * 2,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <TouchableOpacity
+              style={{
+                width: SIZES.width * 0.9,
+                padding: SIZES.padding,
+                backgroundColor: COLORS.primary,
+                alignItems: 'center',
+                borderRadius: SIZES.radius,
+              }}>
+              <Text style={{color: COLORS.white, ...FONTS.h2}}>Order</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   };
